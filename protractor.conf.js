5c3c60fa5bffc2070be98f0dd9bc3ed00c02f067
Tefra <martin.tefra@capgemini.com>
2021-04-28 09:43:00 +0200

    basic login test works

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./e2e_testing/login/login.e2e-spec.ts']
+  specs: ['./e2e_testing/login.js']
 }
