3bc74cf5010e00b537296313c73bcbea1eb4772d
Adrian Hofer <adrian.hofer@capgemini.com>
2021-07-27 16:58:47 +0200

    remove outputs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -44,8 +44,7 @@ export const environment: {
 } = {
   production: false,
   version: 'dev',
-  // api: 'https://forecastr-test.at.capgemini.com/forecastr-api/api/v1/',
-  api: 'https://localhost:62345/api/v1/',
+  api: 'https://forecastr-test.at.capgemini.com/forecastr-api/api/v1/',
   security: 'jwt',
   authPath: 'auth',
   usersPath: 'users',
