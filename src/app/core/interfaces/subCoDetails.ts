ed2d1a21eca6a1101c78e7e04331ccba7a805724
Burak Konca <burak.konca@capgemini.com>
2021-09-28 13:45:26 +0200

    changed DTOs.

--- src/app/core/interfaces/subCoDetails.ts
+++ src/app/core/interfaces/subCoDetails.ts
@@ -1,13 +1,17 @@
 export class subCoDetails {
     subCoId: number;
+    subCoTypeId: number;
     subCoType: string;
     resourceName: string;
     projectName: string;
+    projectId: number;
     customer: string;
     monthId: number;
     forecastId: number;
     manDay: number;
     revenue: number;
+    costRate: number;
+    cor:number;
     cost: number;
     contribution: number;
     cp: number;
@@ -15,14 +19,18 @@ export class subCoDetails {
 
     constructor() {
         this.subCoId = null;
+        this.subCoTypeId = null;
         this.subCoType = '';
 	    this.resourceName = '';	
         this.projectName = '';
+        this.projectId = null;
         this.customer = '';
         this.monthId = null;
         this.forecastId = null;
         this.manDay = null;
         this.revenue = null;
+        this.cor = null;
+        this.costRate = null;
         this.cost = null;
         this.contribution = null;
         this.cp = null;
--- src/app/core/interfaces/subCoPreview.ts
+++ src/app/core/interfaces/subCoPreview.ts
@@ -1,11 +1,13 @@
 export class subCoPreview {
     subCoId: number;
+    subCoTypeId: number;
     subCoType: string;
     resourceName: string;
 
 
     constructor() {
         this.subCoId = null;
+        this.subCoTypeId = null;
         this.subCoType = '';
 	    this.resourceName = '';		
 	}
