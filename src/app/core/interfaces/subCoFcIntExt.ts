77fba572b05e64b2c860a31543528c93956e0270
Burak Konca <burak.konca@capgemini.com>
2021-10-05 15:21:55 +0200

    added DTOs for subco FC view

---
+++ src/app/core/interfaces/subCoFcIntExt.ts
@@ -0,0 +1,31 @@
+export class SubCoFcIntExt{
+    resourceName: string;
+    projectCode: string;
+    projectName: string;
+    engagementManagerId: number;
+    customer: string;
+    isEasternEurope: boolean;
+    revenue: number;
+    cost: number;
+    contribution: number;
+    cP: number;
+
+
+    constructor(){
+        this.resourceName = "";
+        this.projectName = "";
+        this.projectName = "";
+        this.engagementManagerId = null;
+        this.isEasternEurope = false;
+        this.customer = "";
+        this.revenue = 0;
+        this.cost = 0;
+        this.cost = 0;
+        this.contribution = 0;
+        this.cP = 0;
+    }
+}
+
+
+
+   
---
+++ src/app/core/interfaces/subCoFcOffshore.ts
@@ -0,0 +1,19 @@
+export class SubCoFcOffshore{
+    totalRevenue: number;
+    totalCost: number;
+    totalContribution: number;
+    cP: number;
+   
+
+
+    constructor(){
+        this.totalRevenue = 0;
+        this.totalContribution=0;
+        this.totalCost = 0;
+        this.cP = 0;
+    }
+}
+
+
+
+   
