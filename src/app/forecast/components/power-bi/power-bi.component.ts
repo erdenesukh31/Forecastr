ac1456d5a15f9230559346d7af0de2e7214c8d8a
Adrian Hofer <adrian.hofer@capgemini.com>
2021-11-24 13:02:01 +0100

    add power-bi component

---
+++ src/app/forecast/components/power-bi/power-bi.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-power-bi",
+    templateUrl: "./power-bi.component.html",
+    styleUrls: ["./power-bi.component.scss"],
+  })
+  export class PowerBi implements OnInit {
+      
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+      
+  }
