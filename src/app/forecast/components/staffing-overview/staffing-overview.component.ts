6d0453ce565c4a4e1b77de3fb75932dfc3b558fc
Tanja Buczkowski <tanja.buczkowski@capgemini.com>
2022-05-23 12:15:54 +0200

    display grad in HOP/Staffing view and csv exports

--- src/app/forecast/components/staffing-overview/staffing-overview.component.ts
+++ src/app/forecast/components/staffing-overview/staffing-overview.component.ts
@@ -13,6 +13,7 @@ import { Subscription } from "rxjs";
 import { Project } from "../../../core/interfaces/project";
 import { UtilitiesService } from "../../../core/services/utilities.service";
 import { parseHostBindings } from "@angular/compiler";
+import { Grade } from "../../../core/interfaces/grade";
 
 class ProjectHelper {
     projectId: number;
@@ -283,12 +284,16 @@ export class StaffingOverviewComponent implements OnInit, OnDestroy, OnChanges {
         return returnString;
     }
 
+    getGrade(gradeId: number): string {
+        return  this.userService.getGrades().find((g: Grade) => g.gradeId === gradeId) ? this.userService.getGrades().find((g: Grade) => g.gradeId === gradeId).name : '-';
+      }
     initStaffing(): void {
         this.projects = this.utilityService.getProjects();
         this.columnsToDisplay = [];
         this.columnsToDisplay.push('name');
         this.columnsToDisplay.push('team');
         this.columnsToDisplay.push("corp");
+        this.columnsToDisplay.push("grade");
         this.columnsToDisplay.push("projects");
 
         for (let month of this.months) {
@@ -310,7 +315,7 @@ export class StaffingOverviewComponent implements OnInit, OnDestroy, OnChanges {
         this.pageState.showSpinner();
 
         let lineEnding = "\r\n";
-        let header: string = "Employee;Team;Corp Id;" + this.months.map(x => x.name + " ARVE;" + x.name + " FTE").join(";") + lineEnding;
+        let header: string = "Employee;Team;Corp Id;Grade;" + this.months.map(x => x.name + " ARVE;" + x.name + " FTE").join(";") + lineEnding;
 
         let body = "";
 
@@ -340,6 +345,7 @@ export class StaffingOverviewComponent implements OnInit, OnDestroy, OnChanges {
             body += teams.get(team).map(u => u.lastName + ", " + u.firstName + ";" +
                 team + ";" +
                 u.globalId.toString() + ";" +
+                this.getGrade(u.gradeId)  + ";" +
                 this.months.map(x => this.parseForCSV(this.getMonthARVEFromPerson(x, u), 100, 4, 4) + ";" +
                     this.parseForCSV(this.getMonthFTEFromPerson(x, u), 1, 0)).join(";")).join(lineEnding);
             body += lineEnding + lineEnding;
